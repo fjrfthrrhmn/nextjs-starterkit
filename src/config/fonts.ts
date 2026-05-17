ace722aca26d3d8367a732311a3c8d3565948093
Jessen <fjr.fthrrhmn25@gmail.com>
2026-05-17 12:22:08 +0700

    Fix

--- src/config/fonts.ts
+++ src/config/fonts.ts
@@ -1,13 +1,18 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
+	variable: "--font-sans",
+	subsets: ["latin"]
+})
+
+const geistSerif = Geist({
+	variable: "--font-serif",
 	subsets: ["latin"]
 })
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
+	variable: "--font-mono",
 	subsets: ["latin"]
 })
 
-export const fontVariables = `${geistSans.variable} ${geistMono.variable}`
+export const fontVariables = `${geistSans.variable} ${geistMono.variable} ${geistSerif.variable}`

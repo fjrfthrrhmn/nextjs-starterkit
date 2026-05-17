f7b3113c17fb4221c9c2cbc78885970a66b58dca
Jessen <fjr.fthrrhmn25@gmail.com>
2026-05-17 11:59:29 +0700

    chore: Fix formatting, add ci format step, add test

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,11 +1,11 @@
 export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "type-enum": [
-      2,
-      "always",
-      ["feat", "fix", "refactor", "docs", "chore", "style", "test", "perf"],
-    ],
-    "scope-empty": [2, "never"],
-  },
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			["feat", "fix", "refactor", "docs", "chore", "style", "test", "perf"]
+		],
+		"scope-empty": [2, "never"]
+	}
 }

88566a05885150b4e2f8d60c4c585775b6518e50
Jessen <fjr.fthrrhmn25@gmail.com>
2026-05-17 11:09:32 +0700

    refactor: Reorganize app config and clean up import aliases

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,3 @@
 // Application configuration barrel
-// Export config modules saat sudah dibuat:
-// export { env } from "./env"
-// export { siteConfig } from "./site"
-// export { navConfig } from "./nav"
-// export { apiConfig } from "./api"
+export { fontVariables } from "./fonts"
+export { siteMetadata } from "./site"
